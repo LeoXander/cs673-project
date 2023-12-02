00cec4fba5b3e1993d0926a473c3434dac8e7456
sprcoder <sr2484@njit.edu>
2023-12-02 10:55:23 -0500

    Echoing the tnsnames and ewallet during container creation

--- community_activity/startup.sh
+++ community_activity/startup.sh
@@ -3,5 +3,6 @@ date
 
 echo $EWALLET > ewallet.pem
 echo $TNS_NAMES > tnsnames.ora
+cat ewallet.pem
 cat tnsnames.ora
 gunicorn main:app --bind :$PORT --workers 4 --threads 8 --timeout 0 --worker-class uvicorn.workers.UvicornWorker
