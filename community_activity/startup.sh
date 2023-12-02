cd02632cc5f206b5fee7fdf43dc90390d7e15749
sprcoder <sr2484@njit.edu>
2023-12-02 11:34:45 -0500

    Modified with encoded wallet

--- community_activity/startup.sh
+++ community_activity/startup.sh
@@ -2,7 +2,7 @@ echo $HOME
 date
 
 echo $EWALLET | base64 -d > ewallet.pem
-echo $TNS_NAMES | base64 -d > tnsnames.ora
+echo $TNS_NAMES > tnsnames.ora
 cat ewallet.pem
 cat tnsnames.ora
 gunicorn main:app --bind :$PORT --workers 4 --threads 8 --timeout 0 --worker-class uvicorn.workers.UvicornWorker
