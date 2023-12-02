5077fb8a1e5abfa3c7f6e6d506acf01a804f5143
sprcoder <sr2484@njit.edu>
2023-12-01 19:45:49 -0500

    Echoing the pem and ora during container creation

--- community_activity/startup.sh
+++ community_activity/startup.sh
@@ -3,4 +3,6 @@ date
 
 echo $EWALLET > ewallet.pem
 echo $TNS_NAMES > tnsnames.ora
+echo ewallet.pem
+echo tnsnames.ora
 gunicorn main:app --bind :$PORT --workers 4 --threads 8 --timeout 0 --worker-class uvicorn.workers.UvicornWorker
