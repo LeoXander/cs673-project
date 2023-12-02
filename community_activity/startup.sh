272a462ad3b91ae3ec75feed97ee8bbaf57151b0
sprcoder <sr2484@njit.edu>
2023-12-02 11:40:44 -0500

    Finalized the docker implementation

--- community_activity/startup.sh
+++ community_activity/startup.sh
@@ -1,8 +1,3 @@
-echo $HOME
-date
-
 echo $EWALLET | base64 -d > ewallet.pem
 echo $TNS_NAMES | base64 -d > tnsnames.ora
-cat ewallet.pem
-cat tnsnames.ora
 gunicorn main:app --bind :$PORT --workers 4 --threads 8 --timeout 0 --worker-class uvicorn.workers.UvicornWorker
